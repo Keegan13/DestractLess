9a0e11a25c858b0bf3b030d67226633b15d0211d
Andrew <a13andrew@gmail.com>
2018-12-24 17:03:05 +0200

    save work

---
+++ DestractLess/LabelTimer.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace DestractLess
+{
+    public enum TimerState
+    {
+        Running,
+        Unstarted,
+        Paused,
+        Finished,
+        Faulted
+    }
+    public class LabelTimer : Label
+    {
+
+
+        public TimeSpan Timer { get { return _timer; } set { SetTimer(value); } }
+
+        public TimerState State { get; protected set; }
+
+        //
+        private TimeSpan _timer;
+        protected CancellationTokenSource cts;
+        private object locker = new object();
+        // that's a thing
+        private Task _waiter { get; set; }
+
+        public LabelTimer() : this(0, 0, 0)
+        {
+
+        }
+
+        public LabelTimer(int hours, int minutes, int secods) : this(new TimeSpan(hours, minutes, secods))
+        {
+
+        }
+
+        public LabelTimer(TimeSpan timer)
+        {
+            SetTimer(timer);
+            State = TimerState.Unstarted;
+        }
+
+
+        public void SetTimer(int hours, int minutes, int second)
+        {
+            SetTimer(new TimeSpan(hours, minutes, second));
+        }
+
+        private void SetTimer(TimeSpan timer)
+        {
+            if (State == TimerState.Running || State == TimerState.Paused)
+            {
+                Reset();
+            }
+            _timer = timer;
+            SetLabel(_timer);
+        }
+        protected void SetLabel(TimeSpan timer)
+        {
+            lock (locker)
+            {
+                if (timer != null)
+                    this.Content = timer.ToString(@"hh\:mm\:ss\.ffff");
+            }
+        }
+
+        public void Reset()
+        {
+            cts?.Cancel();
+            cts = new CancellationTokenSource();
+            
+            State = TimerState.Unstarted;
+            SetLabel(this._timer);
+        }
+
+        public void Stop()
+        {
+            switch (State)
+            {
+                case TimerState.Running:
+                    this._waiter = new Task(() => { });
+                    State = TimerState.Paused;
+                    break;
+                default: break;
+            }
+
+        }
+
+        public void Start(Action callback)
+        {
+            switch (State)
+            {
+                case TimerState.Unstarted:
+                case TimerState.Finished:
+                case TimerState.Faulted:
+
+                    cts = new CancellationTokenSource();
+                    var token = cts.Token;
+
+                    Task Main = Task.Factory.StartNew((x) =>
+                    {
+                        var label = x as LabelTimer;
+                        Stopwatch s = new Stopwatch();
+                        label.State = TimerState.Running;
+                        s.Start();
+                        while (label._timer > s.Elapsed)
+                        {
+                            if (label.State == TimerState.Paused)
+                            {
+                                s.Stop();
+                                label._waiter.Wait();
+                                label.State = TimerState.Running;
+                                s.Start();
+                            }
+
+                            if (token.IsCancellationRequested)
+                            {
+                                s.Stop();
+                                Dispatcher.Invoke(() =>
+                                {
+                                    label.SetLabel(label.Timer);
+                                });
+                                token.ThrowIfCancellationRequested();
+                            }
+
+                            Dispatcher.Invoke(() =>
+                            {
+                                label.SetLabel(label.Timer - s.Elapsed);
+                            });
+                            Task.Delay(100).Wait();
+                        }
+                        s.Stop();
+                        Dispatcher.Invoke(() =>
+                        {
+                            label.SetLabel(TimeSpan.FromTicks(0));
+                        });
+                    }, this, token);
+
+                    Task handler = Main.ContinueWith((prev) =>
+                    {
+                        State = TimerState.Finished;
+                        if (prev.IsFaulted)
+                        {
+                            this.State = TimerState.Faulted;
+                        }
+                        Dispatcher.Invoke(callback);
+                    });
+                    break;
+                case TimerState.Paused:
+                    this._waiter?.Start();
+                    break;
+                default: break;
+            }
+        }
+
+
+    }
+}
--- DestractLess/MainWindow.xaml.cs
+++ DestractLess/MainWindow.xaml.cs
@@ -12,17 +12,162 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
 
 namespace DestractLess
 {
+
+    public enum ButtonState
+    {
+        Create,
+        Paused,
+        Ready,
+        Running,
+        Finished
+    }
+
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// TODO:   create dynamic icon
+    ///         focus, and close to tray
     public partial class MainWindow : Window
     {
+        private NotifyIcon trayIcon;
+        private ButtonState btnState = ButtonState.Create;
+        private int _previousWindowState = -1;
+
+
         public MainWindow()
         {
             InitializeComponent();
+            trayIcon = new NotifyIcon();
+            trayIcon.Icon = new System.Drawing.Icon(System.IO.Path.Combine(Directory.GetCurrentDirectory(), "tray.ico"));
+
+            trayIcon.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(DoubleClick);
+            this.StateChanged += new EventHandler(StateChangedHandler);
+
+            btnState = ButtonState.Create;
+            this.button.Content = "Set timer";
+
+        }
+
+        public void TimerCallBack()
+        {
+
+            if (this.WindowState == WindowState.Minimized)
+            {
+                this._previousWindowState = (int)WindowState;
+                this.WindowState = WindowState.Normal;
+            }
+
+            this.Show();
+            this.Activate();
+            this.Focus();
+            this.Topmost = true;
+            this.Topmost = false;
+
+
+            btnState = ButtonState.Finished;
+            button.Content = "Continue";
+            label_t.Reset();
+
+
+            //switch (this.label_t.State)
+            //{
+            //    case TimerState.Finished:
+
+
+            //        break;
+            //    case TimerState.Faulted:break;
+
+            //    default: break;
+
+            //}
+        }
+
+        private void StateChangedHandler(object sender, EventArgs e)
+        {
+            if (this.WindowState == WindowState.Minimized)
+            {
+                this.ShowInTaskbar = false;
+                trayIcon.BalloonTipTitle = "Minimize Sucessful";
+                trayIcon.BalloonTipText = "Minimized the app ";
+                trayIcon.ShowBalloonTip(400);
+                trayIcon.Visible = true;
+
+            }
+            if (this.WindowState == WindowState.Normal)
+            {
+                trayIcon.Visible = false;
+                this.ShowInTaskbar = true;
+            }
+        }
+
+        private void DoubleClick(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            this.WindowState = WindowState.Normal;
+            //this.ShowInTaskbar = true;
+            //this.Show();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            switch (btnState)
+            {
+                case ButtonState.Create:
+                    if (int.TryParse(this.Hours.Text, out int hours) &&
+                        int.TryParse(this.Minutes.Text, out int minutes) &&
+                        int.TryParse(this.Seconds.Text, out int seconds))
+                    {
+                        this.label_t.SetTimer(hours, minutes, seconds);
+                        this.button.Content = "Start";
+                        btnState = ButtonState.Ready;
+                    }
+                    break;
+                case ButtonState.Ready:
+                case ButtonState.Paused:
+                case ButtonState.Finished:
+                    this.label_t.Start(this.TimerCallBack);
+                    this.btnState = ButtonState.Running;
+                    this.button.Content = "Pause";
+                    if (_previousWindowState > 0)
+                    {
+                        this.WindowState = (WindowState)_previousWindowState;
+                        _previousWindowState = -1;
+                    }
+                    break;
+                case ButtonState.Running:
+                    this.label_t.Stop();
+                    this.btnState = ButtonState.Paused;
+                    this.button.Content = "Continue";
+                    break;
+                default: break;
+            }
+        }
+
+        private void Timer_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var textbox = sender as System.Windows.Controls.TextBox;
+            if (this.button != null)
+            {
+                if (Regex.IsMatch(textbox.Text, @"^[0-9]{1,2}$") && int.TryParse(textbox.Text, out int result) && result < 60)
+                {
+
+                }
+                else
+                {
+                    textbox.Text = "0";
+                }
+
+                this.button.Content = "Restart";
+                this.btnState = ButtonState.Create;
+
+            }
         }
     }
 }
